388a76b51750fd568aacbb016420236b30a17450
alexsant123 <eua2016@hotmail.com>
2025-03-14 23:25:30 -0300

    add new features

--- src/main/java/estoque/service/DashboardService.java
+++ src/main/java/estoque/service/DashboardService.java
@@ -9,11 +9,7 @@ import estoque.repository.SaidaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 
 @Service
